c4f163c58d554dc74aaf5e35cd2f8e8f9e16e4b0
Swapnil Kunkekar <swapnil93kunkekar@gmail.com>
2024-07-25 14:54:29 +0530

    Initial Commit
    Initial Commit

---
+++ MyFristWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFristWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
