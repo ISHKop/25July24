e7ad11e8cc92c700eb01dd068d098e39c5bb8050
Swapnil Kunkekar <swapnil93kunkekar@gmail.com>
2024-07-31 14:55:22 +0530

    Created Villa Project and integrated HTML Theme

---
+++ Villla/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villla.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
